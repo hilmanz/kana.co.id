cc36d0b21fc866893aca4c23d603396f86246846
Rangga Herly Hilman Herawan <rangga.hilmanz@gmail.com>
2017-07-27 16:43:33 +0700

    BACKUP
    BACKUP

---
+++ testemail.php
@@ -0,0 +1,26 @@
+<?
+
+// Reporting E_NOTICE can be good too (to report uninitialized
+// variables or catch variable name misspellings ...)
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+// Report all errors except E_NOTICE
+error_reporting(E_ALL & ~E_NOTICE);
+
+// Report all PHP errors (see changelog)
+error_reporting(E_ALL);
+
+// Report all PHP errors
+error_reporting(-1);
+
+// Same as error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+
+	curl "https://api.postmarkapp.com/email" \
+  -X POST \
+  -H "Accept: application/json" \
+  -H "Content-Type: application/json" \
+  -H "X-Postmark-Server-Token: 580e959d-8b28-4f90-b741-4b6765b49f7d" \
+  -d "{From: 'info@kana.co.id', To: 'ridwan@kana.co.id', Subject: 'New Web Lead', HtmlBody: 'test'}"
+
+  ?>
